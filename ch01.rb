85ca617ea07cdedd8f52261a06b75112cc24a861
Carlos Agarie <carlos.agarie@gmail.com>
2015-02-20 16:55:11 -0200

    Put the chapter 01 code in a module

--- ch01.rb
+++ ch01.rb
@@ -1,88 +1,90 @@
-class Buffer
-  attr_accessor :queued, :wip
+module ControlTheory
+  class Buffer
+    attr_accessor :queued, :wip
 
-  def initialize(max_wip, max_flow)
-    @queued = 0
-    @wip = 0 # Work-in-progress or "ready pool".
+    def initialize(max_wip, max_flow)
+      @queued = 0
+      @wip = 0 # Work-in-progress or "ready pool".
 
-    @max_wip = max_wip
-    @max_flow = max_flow
-  end
+      @max_wip = max_wip
+      @max_flow = max_flow
+    end
 
-  def work(u)
-    # Add to ready pool.
-    u = [0, u.round].max
-    u = [u, @max_wip].min
-    @wip += u
+    def work(u)
+      # Add to ready pool.
+      u = [0, u.round].max
+      u = [u, @max_wip].min
+      @wip += u
 
-    # Transfer from ready pool to queue.
-    r = Random.rand(0..@wip)
-    @wip -= r
-    @queued += r
+      # Transfer from ready pool to queue.
+      r = Random.rand(0..@wip)
+      @wip -= r
+      @queued += r
 
-    # Release from queue to downstream process.
-    r = Random.rand(0..@max_flow)
-    r = [r, @queued].min
-    @queued -= r
+      # Release from queue to downstream process.
+      r = Random.rand(0..@max_flow)
+      r = [r, @queued].min
+      @queued -= r
 
-    @queued
-  end
-end
-
-class Controller
-  attr_accessor :i, :kp, :ki
-
-  def initialize(kp, ki)
-    @kp = kp
-    @ki = ki
-    @i = 0
-  end
-
-  def work(e)
-    @i += e
-    @kp * e + @ki * @i
-  end
-end
-
-def open_loop(p, tm = 5000)
-  target = Proc.new { |t| 5.0 }
-
-  tm.times do |t|
-    u = target.call(t)
-    y = p.work(u)
-
-    puts t, u, 0, u, y
-  end
-end
-
-def closed_loop(c, p, tm=5000)
-  setpoint = Proc.new do |t|
-    case t
-    when t < 100
-      0
-    when t >= 100 && t < 300
-      50
-    else
-      10
+      @queued
     end
   end
 
-  y = 0
-  tm.times do |t|
-    r = setpoint.call(t)
-    e = r - y
-    u = c.work(e)
-    y = p.work(u)
+  class Controller
+    attr_accessor :i, :kp, :ki
 
-    puts t, r, e, u, y
+    def initialize(kp, ki)
+      @kp = kp
+      @ki = ki
+      @i = 0
+    end
+
+    def work(e)
+      @i += e
+      @kp * e + @ki * @i
+    end
+  end
+
+  def self.open_loop(p, tm = 5000)
+    target = Proc.new { |t| 5.0 }
+
+    (1..tm).each do |t|
+      u = target.call(t)
+      y = p.work(u)
+
+      puts "#{t},#{u},0,#{u},#{y}"
+    end
+  end
+
+  def self.closed_loop(c, p, tm=5000)
+    setpoint = Proc.new do |t|
+      case t
+      when t < 100
+        0
+      when t >= 100 && t < 300
+        50
+      else
+        10
+      end
+    end
+
+    y = 0
+    (1..tm).each do |t|
+      r = setpoint.call(t)
+      e = r - y
+      u = c.work(e)
+      y = p.work(u)
+
+      puts "#{t},#{r},#{e},#{u},#{y}"
+    end
   end
 end
 
-c = Controller.new(1.25, 0.01)
-p = Queue.new
+c = ControlTheory::Controller.new(1.25, 0.01)
+p = ControlTheory::Buffer.new(10, 3)
 
 puts "OPEN LOOP"
-open_loop(p, 1000)
+ControlTheory.open_loop(p, 1000)
 
 puts "CLOSED LOOP"
-closed_loop(c, p, 1000)
+ControlTheory.closed_loop(c, p, 1000)
